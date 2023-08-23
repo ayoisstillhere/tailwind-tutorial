ac9a481aa6ae39db01c84d4042f703416d18c00a
Ayodele Fagbami <afagbami@gmail.com>
2023-08-23 21:14:55 +0100

    added button and improved responsiveness

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -11,17 +11,19 @@ const Hero = () => {
           Grow With Coding.
         </h1>
         <div className="flex justify-center items-baseline">
-          <p className="md:text-5xl sm:text-4xl text-xl font-bold">
+          <p className="md:text-5xl sm:text-4xl text-xl font-bold py-4">
            Practice coding for
           </p>
           <Typed
-            className="md:text-5xl sm:text-4xl text-xl font-bold pl-2"
+            className="md:text-5xl sm:text-4xl text-xl font-bold pl-2 text-gray-500 md:pl-4"
             strings={["jobs", "money", "fun"]}
             typeSpeed={120}
             backSpeed={140}
             loop
           />
         </div>
+        <p className="md:text-2xl text-xl font-bold text-gray-500">Improve your skills every day and you'll have a great career ahead.</p>
+        <button className="bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3 text-black">Get Started</button>
       </div>
     </div>
   );
