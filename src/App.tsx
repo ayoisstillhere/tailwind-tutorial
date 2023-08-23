ce25e0ae2dbdf1cb11a3ff935beace95ce7a2afb
Ayodele Fagbami <afagbami@gmail.com>
2023-08-23 17:25:11 +0100

    navbat logo and icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import Navbar from "./components/Navbar";
 
 function App() {
-
   return (
-    <div></div>
-  )
+    <div>
+      <Navbar />
+    </div>
+  );
 }
 
-export default App
+export default App;
